2045089ed61c94daf88f84705251fd4cc1da176c
Alef Sloan <alef_sloan@hotmail.com>
2022-12-21 01:23:26 -0300

    feat: create SearchBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Header } from "./components/Header/Header";
+import { SearchBar } from "./components/SearchBar/SearchBar";
+
+import style from './App.module.css';
 
 export function App() {
   return (
-    <>
+    <div>
       <Header />
-    </>
+      <div className={style.wrapper}>
+        <SearchBar />
+      </div>
+    </div>
   )
 }
